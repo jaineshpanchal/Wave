b83b4951e323fd876294158803071903fb9391ce
jaineshpanchal <jaineshpanchal@gmail.com>
2025-04-12 15:31:20 -0400

    codebase updated

---
+++ Wave/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Wave
+//
+//  Created by Jainesh Panchal on 4/12/25.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+
+    func application(_ application: UIApplication,
+                     didReceiveRemoteNotification userInfo: [AnyHashable : Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        if Auth.auth().canHandleNotification(userInfo) {
+            completionHandler(.noData)
+            return
+        }
+        completionHandler(.newData)
+    }
+}
--- Wave/WaveApp.swift
+++ Wave/WaveApp.swift
@@ -6,30 +6,15 @@
 //
 
 import SwiftUI
-import Firebase
-import FirebaseAppCheck
 
 @main
 struct WaveApp: App {
-    init() {
-        // Enable debug mode
-        UserDefaults.standard.set(true, forKey: "FirebaseAppCheckDebugMode")
-        print("🔑 App Check Debug Mode is ON")
-
-        FirebaseApp.configure()
-
-        // Use debug provider instead of App Attest for development
-        AppCheck.setAppCheckProviderFactory(DebugAppCheckProviderFactory())
-
-        print("🚀 Firebase and App Check (Debug Provider) initialized")
-    }
+    // 👇 Inject the delegate here
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
         WindowGroup {
-            PhoneAuthView()
+            ContentView()
         }
     }
 }
-
-
-
