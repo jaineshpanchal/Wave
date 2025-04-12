11d9a08e986c142c10faf9e16c8495173a7cfd41
jaineshpanchal <jaineshpanchal@gmail.com>
2025-04-12 20:44:11 +0000

    file updated

--- Wave/AppDelegate.swift
+++ Wave/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
-        AppCheck.setAppCheckProviderFactory(AppCheckProviderFactory())
+        AppCheck.setAppCheckProviderFactory(AppCheckProviderFactory() as any AppCheckProviderFactory)
         return true
     }
 
